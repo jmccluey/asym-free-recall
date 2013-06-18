d21c2c9f9fa05f587e656031cfb5dd8726cfe038
Josh McCluey <jmccluey@users.noreply.github.com>
2013-06-18 16:24:06 +0000

    updated to version 2

--- exp/asymFR/asymFR.py
+++ exp/asymFR/asymFR.py
@@ -60,27 +60,50 @@ def prepare(exp, config):
     vsn.write(svn_version.read())
     vsn.close()
 
+    # set category order for this subject
+    (subjCatOrder, subjCatNames) = prep.subjCatOrder(exp, config)
+    
     # set word order for this subject
-    (wp, subjItems, subjListOrder) = prep.subjWordOrder(exp, config)
+    (wp, wptot, wppract) = prep.extractPools(config.poolDir, config.namefile, config.practfile)
+    
+    subjItems = prep.subjWordOrder(subjCatOrder, wp, wppract)
 
     # write out all the to-be-presented items to text files
     for i in range(config.nSessions):
         exp.setSession(i)
+        if i == 0:
+            # write practice lists
+            for j in xrange(config.nPractLists):
+                # each practice list written to data/[subject]/session[i]_/p[j].lst
+                listFile = exp.session.createFile('p%d.lst' % j )
+
+                for k in xrange(config.listLength):
+                    # one word per line
+                    listFile.write('%s\n' % subjItems[i][j][k])
+                listFile.close()
+        
         for j in xrange(config.nLists):
             # each list written to data/[subject]/session_[i]/[j].lst
-            listFile = exp.session.createFile('%d.lst' % j)
+            listFile = exp.session.createFile('%d.lst' % j )
 
-            # one word per line
-            for k in xrange(config.listLength):
-                listFile.write('%s\n' % subjItems[i][j][k])
-            listFile.close()
+            if i == 0:
+                # account for practice lists
+                for k in xrange(config.listLength):
+                    # one word per line
+                    listFile.write('%s\n' % subjItems[i][j+config.nPractLists][k])
+                listFile.close()
+            else:
+                for k in xrange(config.listLength):
+                    # one word per line
+                    listFile.write('%s\n' % subjItems[i][j][k])
+                listFile.close()             
 
     # create a number of math problem sets, one for each list
     terms = []
     ops = []
     answers = []
     proposed = []
-    for i in range(config.nLists):
+    for i in range(config.nLists+config.nPractLists):
         set_config = math.prep_math_set(config.numVars,
             config.minNum, config.maxNum, config.maxProbs, 
             config.plusAndMinus, config.ansMod, config.ansProb,
@@ -94,8 +117,11 @@ def prepare(exp, config):
     # save the prepared data; set state to first session, trial
     exp.saveState(state,
                   wp=wp,
+                  wptot=wptot,
+                  wppract=wppract,
                   subjItems=subjItems,
-                  subjListOrder=subjListOrder,
+                  subjCatOrder=subjCatOrder,
+                  subjCatNames=subjCatNames,
                   sessionNum=0,
                   trialNum=0,
                   blockNum=0,
@@ -103,7 +129,8 @@ def prepare(exp, config):
                   ops=ops,
                   answers=answers,
                   proposed=proposed,
-                  tcorrect=0)
+                  tcorrect=0,
+                  pract=True)
 
 def formatStr(format, input):
     """
@@ -117,9 +144,12 @@ def formatStr(format, input):
 
 def logEvent(log, ts, type,
              trialno=None,
-             listname=None,
              item=None,
-             itemno=None):
+             itemno=None,
+             category=None,
+             catno=None,
+             resp=None,
+             rt=None):
     """
     Use standard formatting to log an event in session.log.
 
@@ -133,21 +163,56 @@ def logEvent(log, ts, type,
     trialno : int
     item : str
     itemno : int
+    category : str
+    catno : int
+    resp : int
+    rt : int
     """
 
     # format each input. If any input is None, it will be formatted
     # as an empty string.
     inputs = (formatStr('%s', type),
               formatStr('%s', trialno),
-              formatStr('%s', listname),
               formatStr('%s', item),
-              formatStr('%s', itemno))
+              formatStr('%s', itemno),
+              formatStr('%s', category),
+              formatStr('%s', catno),
+              formatStr('%s', resp),
+              formatStr('%s', rt))
 
     lineFormat = '%s\t' * (len(inputs) - 1) + '%s'
     log.logMessage(lineFormat % inputs, ts)
 
+def subjectError(errType, duration,
+                 video=None,
+		 config=None,
+                 beep=None,
+                 log=None):
+    """
+    Display a warning if a participant makes a mistake.
+    """
+
+    if errType=='slow':
+        errMsg = "Please try to respond quickly"
+        logMsg = 'SLOW'
+    else:
+        errMsg = "Unknown error occured"
+        logMsg = 'ERROR'
+
+    # beep at them
+    if beep:
+        beep.present()
+    # display a message
+    if video:
+        video.updateScreen()
+	stim = Text(errMsg, size=config.wordHeight)
+        timestamp = flashStimulus(stim, duration)
+    # log it
+    if log:
+        log.logMessage(logMsg, timestamp)
+
 def trial(exp, config, clock, state, log, video, audio, mathlog,
-          startBeep, stopBeep, tf_bc, fixationCross):
+          startBeep, stopBeep, resp_bc, tf_bc, fixationCross, practice=False):
     """
     Present a list of words, followed by a free recall period.
 
@@ -180,17 +245,23 @@ def trial(exp, config, clock, state, log, video, audio, mathlog,
     PLD W ISI W ISI ... W ISI PRD R
     """
 
-    # get the list name
-    listname = state.subjListOrder[state.sessionNum][state.trialNum]
-
     # PRESENT THE LIST
     for n in range(config.listLength):
 
         # PREPARE STIMULUS
         # prepare item text
         item = state.subjItems[state.sessionNum][state.trialNum][n]
-        itemInd = state.wp.index(item) + 1 # itemnos are one-indexed
+        cat = state.subjCatNames[state.sessionNum][state.trialNum][n]
+        catno = state.subjCatOrder[state.sessionNum][state.trialNum][n]
         itemText = Text(item, size=config.wordHeight)
+
+        # get itemno
+        if practice:
+            itemInd = state.wppract.index(item) + 1 # itemnos are one-indexed
+            trialNum = -1
+        else:
+            itemInd = state.wptot.index(item) + 1 # itemnos are one-indexed
+            trialNum = state.trialNum - config.nPractLists
 
         # PRESENT STIMULUS
         video.showProportional(itemText,.5,.5)
@@ -199,9 +270,30 @@ def trial(exp, config, clock, state, log, video, audio, mathlog,
         ts = video.updateScreen(clock)
         clock.delay(config.wordDuration)
 
+        # encoding task response
+        b, rt = resp_bc.waitWithTime(None, config.wordDuration)
+
+        errType = None
+        if not b:
+            # participant didn't respond in time
+            resp = -999
+            errType = 'slow'
+        else:
+            # get the pressed button
+            buttonText = b.name
+
+            # translate to response code
+            resp = config.respPool[buttonText]
+
         # log the word 
-        logEvent(log, ts, 'FR_PRES', trialno=state.trialNum, listname=listname,
-                 item=item, itemno=itemInd)
+        logEvent(log, ts, 'FR_PRES', trialno=trialNum, item=item, itemno=itemInd,
+                 category=cat, catno=catno, resp=resp, rt=rt[0]-ts[0])
+
+        if errType:
+            # tell the participant they made a mistake and log it
+            video.clear()
+            video.updateScreen(clock)
+            subjectError(errType, config.msgDur, video, config, startBeep, log)
 
         # show the fixation cross
         video.clear()
@@ -268,8 +360,13 @@ def trial(exp, config, clock, state, log, video, audio, mathlog,
     video.unshow(fix)
 
     # Record responses, log the rec start
+    if practice:
+        recname = 'p%d' % state.trialNum
+    else:
+        recname = str(trialNum)
+    
     (rec, timestamp) = audio.record(config.recallDuration,
-                                    str(state.trialNum),
+                                    recname,
                                     t=clock)
     logEvent(log, timestamp, 'REC_START')
 
@@ -322,6 +419,13 @@ def run(exp, config):
     stopBeep = Beep(config.stopBeepFreq,
                     config.stopBeepDur,
                     config.stopBeepRiseFall)
+
+    # create the button chooser for encoding task responses
+    respbuttons = config.respPool.keys()
+    respkeys = []
+    for b in respbuttons:
+        respkeys.append(Key(b))
+    resp_bc = ButtonChooser(*respkeys)
 
     # create the button chooser for math distraction
     tfkeys = config.tfKeys
@@ -423,9 +527,30 @@ def run(exp, config):
     
     if state.blockNum==0:
         # check if we're still presenting lists
-        while state.trialNum < config.nLists:
-            
-            if state.trialNum > 0:
+        while state.trialNum < config.nPractLists + config.nLists:
+
+            # add an if for done with practice
+            if state.trialNum == config.nPractLists:
+                state.pract = False
+                exp.saveState(state)
+
+                # REPLACE WITH INSTRUCTIONS
+                # minimum break duration
+                breakText = Text(open(config.textFiles['trialBreak'],'r').read())
+                breakStim = video.showCentered(breakText)
+                video.updateScreen(clock)
+                video.unshow(breakStim)
+                clock.delay(config.breakDuration)
+                
+                # after break, wait for participant to continue
+                if config.breakSubjectControl:
+                    endBreakText = Text(open(config.textFiles['endBreak'],'r').read())
+                    timestamp = waitForAnyKey(clock, endBreakText)
+                    
+                    # log break
+                    logEvent(log, timestamp, 'REST')
+                
+            elif state.trialNum > 0:
                 # minimum break duration
                 breakText = Text(open(config.textFiles['trialBreak'],'r').read())
                 breakStim = video.showCentered(breakText)
@@ -451,7 +576,8 @@ def run(exp, config):
             
             # run a trial (a trial is a word list)
             trial(exp, config, clock, state, log, video, audio, mathlog,
-                  startBeep, stopBeep, tf_bc, fixationCross)
+                  startBeep, stopBeep, resp_bc, tf_bc, fixationCross,
+                  practice=state.pract)
             
             # save the state after each trial
             state.trialNum += 1
